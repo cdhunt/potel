3fb6df225f5ef6e189cb954569a6cb91fe524145
Chris Hunt <github@automatedops.com>
2023-12-07 16:10:02 -0500

    Adds Add-ExporterOtlpTrace with tests

---
+++ src/public/Add-ExporterOtlpTrace.ps1
@@ -0,0 +1,63 @@
+function Add-ExporterOtlpTrace {
+    <#
+	.SYNOPSIS
+		Adds an OTLP Exporter
+	.DESCRIPTION
+		Adds OpenTelemetry.Exporter.Console
+	.PARAMETER InputObject
+		Instance of TracerProviderBuilderBase.
+	.INPUTS
+		Instance of TracerProviderBuilderBase
+	.OUTPUTS
+		TracerProviderBuilderBase
+	.EXAMPLE
+		PS> New-TracerBuilder | Add-HttpClientInstrumentation
+	#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = "Trace")]
+        [OpenTelemetry.Trace.TracerProviderBuilderBase]
+        $TracerProvider,
+
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = "Meter" )]
+        [OpenTelemetry.Metrics.MeterProviderBuilderBase]
+        $MeterBuilder,
+
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Endpoint,
+
+        [Parameter(Position = 1)]
+        [hashtable]
+        $Headers,
+
+        [Parameter(Position = 2)]
+        [uint]
+        $Timeout,
+
+        [Parameter(Position = 3)]
+        [ValidateSet('grpc', 'http/protobuf')]
+        [string]
+        $Protocol
+    )
+
+    switch ($PSBoundParameters) {
+        { $_.ContainsKey('Endpoint') } { $env:OTEL_EXPORTER_OTLP_ENDPOINT = $Endpoint }
+        { $_.ContainsKey('Headers') } { $env:OTEL_EXPORTER_OTLP_HEADERS = $Headers.Keys.ForEach({ "$_=$($Headers[$_])" }) -join ',' }
+        { $_.ContainsKey('Timeout') } { $env:OTEL_EXPORTER_OTLP_TIMEOUT = $Timeout }
+        { $_.ContainsKey('Protocol') } { $env:OTEL_EXPORTER_OTLP_PROTOCOL = $Protocol }
+        Default {}
+    }
+
+    $type = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object Location -like "*lib?OpenTelemetry.Exporter.OpenTelemetryProtocol.dll" | Select-Object -Last 1
+
+    switch ($PSBoundParameters) {
+        { $_.ContainsKey('TracerProvider') } {
+            $type.GetType('OpenTelemetry.Trace.OtlpTraceExporterHelperExtensions').GetMethod('AddOtlpExporter', ([System.Reflection.BindingFlags]::Public -bor [System.Reflection.BindingFlags]::Static), [OpenTelemetry.Trace.TracerProviderBuilder]).Invoke($null, @($TracerProvider))
+        }
+
+        { $_.ContainsKey('MeterBuilder') } {
+            $type.GetType('OpenTelemetry.Metrics.OtlpMetricExporterExtensions').GetMethod('AddOtlpExporter', ([System.Reflection.BindingFlags]::Public -bor [System.Reflection.BindingFlags]::Static), [OpenTelemetry.Metrics.MeterProviderBuilder]).Invoke($null, @($MeterBuilder))
+        }
+    }
+}
--- test/Tracer.Tests.ps1
+++ test/Tracer.Tests.ps1
@@ -81,6 +81,59 @@ Describe 'Add-HttpClientInstrumentation' {
     }
 }
 
+Describe 'Add-ExporterOtlpTrace' {
+    BeforeEach {
+        Remove-Item env:\OTEL_EXPORTER_OTLP_ENDPOINT -ErrorAction SilentlyContinue
+        Remove-Item env:\OTEL_EXPORTER_OTLP_HEADERS -ErrorAction SilentlyContinue
+        Remove-Item env:\OTEL_EXPORTER_OTLP_TIMEOUT -ErrorAction SilentlyContinue
+        Remove-Item env:\OTEL_EXPORTER_OTLP_PROTOCOL -ErrorAction SilentlyContinue
+    }
+
+    It 'Should work with TracerProviderBuilder' -Tag @('unit', 'trace') {
+        $result = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/
+        $result.GetType().FullName | Should -be 'OpenTelemetry.Trace.TracerProviderBuilderBase'
+        $env:OTEL_EXPORTER_OTLP_ENDPOINT | Should -Be 'http://tracer/'
+    }
+
+    It 'Should work with MeterProviderBuilder' -Tag @('unit', 'metric') {
+        $result = [OpenTelemetry.Sdk]::CreateMeterProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://meter/
+        $result.GetType().FullName | Should -be 'OpenTelemetry.Metrics.MeterProviderBuilderBase'
+        $env:OTEL_EXPORTER_OTLP_ENDPOINT | Should -Be 'http://meter/'
+    }
+
+    It 'Should set $env:OTEL_EXPORTER_OTLP_HEADERS with Headers parameter' -Tag @('unit', 'trace') {
+        $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Headers @{"header1" = "value1"; "header2" = "value2" }
+        $env:OTEL_EXPORTER_OTLP_HEADERS | Should -BeLikeExactly "*header1=value1*"
+        $env:OTEL_EXPORTER_OTLP_HEADERS | Should -BeLikeExactly "*header2=value2*"
+        $env:OTEL_EXPORTER_OTLP_HEADERS | Should -BeLikeExactly "header?=value?,header?=value?"
+    }
+
+    It 'Should set $env:OTEL_EXPORTER_OTLP_TIMEOUT with Timeout parameter' -Tag @('unit', 'trace') {
+        $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Timeout 100
+        $env:OTEL_EXPORTER_OTLP_TIMEOUT | Should -Be 100
+    }
+
+    It 'Should set throw with a negative Timeout parameter' -Tag @('unit', 'trace') {
+        { $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Timeout -1 } | Should -Throw
+        $env:OTEL_EXPORTER_OTLP_TIMEOUT | Should -BeNullOrEmpty
+    }
+
+    It 'Should set $env:OTEL_EXPORTER_OTLP_PROTOCOL with Protocol parameter (grpc)' -Tag @('unit', 'trace') {
+        $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Protocol grpc
+        $env:OTEL_EXPORTER_OTLP_PROTOCOL | Should -Be 'grpc'
+    }
+
+    It 'Should set $env:OTEL_EXPORTER_OTLP_PROTOCOL with Protocol parameter (http/protobuf)' -Tag @('unit', 'trace') {
+        $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Protocol 'http/protobuf'
+        $env:OTEL_EXPORTER_OTLP_PROTOCOL | Should -Be 'http/protobuf'
+    }
+
+    It 'Should set throw with a bad Protocol parameter' -Tag @('unit', 'trace') {
+        { $null = [OpenTelemetry.Sdk]::CreateTracerProviderBuilder() | Add-ExporterOtlpTrace -Endpoint http://tracer/ -Protocol 'magic' } | Should -Throw
+        $env:OTEL_EXPORTER_OTLP_PROTOCOL | Should -BeNullOrEmpty
+    }
+}
+
 AfterAll {
     Remove-Module potel
 }
