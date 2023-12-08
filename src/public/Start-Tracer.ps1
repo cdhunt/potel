e8366fb796f4a36cd85796e4dcb9d9fd352a1756
Chris Hunt <github@automatedops.com>
2023-12-08 09:40:58 -0500

    Adds Start and Stop Tracer

---
+++ src/public/Start-Tracer.ps1
@@ -0,0 +1,17 @@
+function Start-Tracer {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [OpenTelemetry.Trace.TracerProviderBuilderBase]
+        $InputObject,
+
+        [Parameter()]
+        [switch]$PassThru
+    )
+
+    $global:potel_provider = [OpenTelemetry.Trace.TracerProviderBuilderExtensions]::Build($InputObject)
+
+    if ($PassThru) {
+        Write-Output $potel_provider
+    }
+}
---
+++ src/public/Stop-Tracer.ps1
@@ -0,0 +1,16 @@
+function Stop-Tracer {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [OpenTelemetry.Trace.TracerProvider]
+        $TracerProvider = $global:potel_provider
+
+    )
+
+    $result = [OpenTelemetry.Trace.TracerProviderExtensions]::Shutdown($TracerProvider) # For exporter
+    $TracerProvider.Dispose() # For Instrumentation
+
+    if (!$result) {
+        Write-Warning "Tracer Shutdown operation did not return True"
+    }
+}
