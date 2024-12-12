d8a52bf581056f09fc291efc98e2b058f22815e7
Chris Hunt <github@automatedops.com>
2024-12-12 11:31:41 -0500

    Exclude Microsoft.Bcl.AsyncInterfaces

--- src/internal/Add-PackageTypes.ps1
+++ src/internal/Add-PackageTypes.ps1
@@ -20,7 +20,8 @@ function Add-PackageTypes {
     }
 
     process {
-        foreach ($path in (Get-ChildItem $target | Where-Object { $_.Name -like '*.dll' -and $_.BaseName -ne "grpc_csharp_ext.x64" } | Select-Object -ExpandProperty FullName)) {
+        $files = Get-ChildItem $target | Where-Object { $_.Name -like '*.dll' -and $_.BaseName -ne "grpc_csharp_ext.x64" -and $_.BaseName -ne 'Microsoft.Bcl.AsyncInterfaces' } | Select-Object -ExpandProperty FullName
+        foreach ($path in ($files)) {
             Add-Type -Path $path
         }
     }
